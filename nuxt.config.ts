ebd41b744a8547c3ddc12f3ffcccd9b052f4ce4e
Jeanlee Barreto <jeanlee-roy@hotmail.com>
2025-11-09 17:14:03 -0500

    chore: add Prettier and update formatting rules

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,19 +1,19 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-import tailwindcss from "@tailwindcss/vite";
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineNuxtConfig({
-  compatibilityDate: "2024-11-01",
-  css: ["~/assets/main.css"],
+  compatibilityDate: '2024-11-01',
+  css: ['~/assets/main.css'],
   devtools: { enabled: true },
   nitro: {
     storage: {
       uploads: {
-        driver: "fs",
-        base: "./public/uploads",
+        driver: 'fs',
+        base: './public/uploads',
       },
     },
   },
-  modules: ["@nuxt/eslint", "@nuxt/icon", "@nuxt/image"],
+  modules: ['@nuxt/eslint', '@nuxt/icon', '@nuxt/image'],
   vite: {
     plugins: [tailwindcss()],
   },
--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,7 @@
   <div>
     <h1 class="text-3xl font-bold mb-4">Component</h1>
 
-    <br>
+    <br />
 
     <FileUpload />
   </div>
