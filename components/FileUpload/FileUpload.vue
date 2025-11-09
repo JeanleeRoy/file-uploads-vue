c0736473d9648120b3e31e71fdaa5908dcb10fb9
Jeanlee Barreto <jeanlee-roy@hotmail.com>
2025-11-09 03:40:50 -0500

    feat: initial file upload component with file selection handling

--- components/FileUpload/FileUpload.vue
+++ components/FileUpload/FileUpload.vue
@@ -1,5 +1,29 @@
+<script setup lang="ts">
+import { ref } from "vue";
+
+const files = ref<File[]>([]);
+
+const handleFileSelected = (e: Event) => {
+  const target = e.target as HTMLInputElement;
+  if (!target.files) return;
+
+  const selectedFiles = target.files;
+  files.value = Array.from(selectedFiles);
+  console.log("Selected files:", files.value);
+};
+</script>
+
 <template>
-  <div>
-    <!-- follow along here -->
+  <label class="block border w-full max-w-3xl p-3 rounded cursor-pointer">
+    <input type="file" class="hidden" @change="handleFileSelected" />
+    <p class="text-center text-gray-500">Upload files</p>
+  </label>
+
+  <div class="mt-3">
+    <ul>
+      <li v-for="file in files" :key="file.name">
+        {{ file.name }}
+      </li>
+    </ul>
   </div>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,9 @@
 <template>
   <div>
     <h1 class="text-3xl font-bold mb-4">Component</h1>
+
+    <br>
+
+    <FileUpload />
   </div>
 </template>
